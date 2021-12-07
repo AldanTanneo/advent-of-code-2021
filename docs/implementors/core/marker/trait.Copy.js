0f641a278f07880cb16959af8f60706d6a40079e
AldanTanneo <aldantanneo@gmail.com>
2021-12-07 01:21:57 +0100

    updated docs

---
+++ docs/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["advent_of_code_2021"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"advent_of_code_2021/challenges/day04/struct.BingoNumber.html\" title=\"struct advent_of_code_2021::challenges::day04::BingoNumber\">BingoNumber</a>","synthetic":false,"types":["advent_of_code_2021::challenges::day04::BingoNumber"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
